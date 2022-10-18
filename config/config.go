e839f244e849bbdb5411a89292e7ef1a0925c6a6
cannjo <113037392+cannjo@users.noreply.github.com>
2022-10-18 13:11:20 +0200

    Add files via upload

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func SetupConfig() {
+	viper.SetConfigName("config")          // name of config file (without extension)
+	viper.SetConfigType("yaml")            // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("$HOME/.beetroot") // call multiple times to add many search pathsw
+	viper.AddConfigPath(".")               // optionally look for config in the working directory
+
+	if err := viper.ReadInConfig(); err != nil { // Handle errors reading the config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Fatalf("Error cannot find config file %s\n", err.Error())
+		}
+	}
+}
+
+func ReadData() {
+	fmt.Println(viper.Get("Hacker"))
+}
